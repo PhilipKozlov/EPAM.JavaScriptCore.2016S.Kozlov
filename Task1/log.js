a02782b5f4310d367013cedebd42297cd270bb48
PhilipKozlov <philip.g.kozlov@gmail.com>
2016-08-10 15:01:42 +0300

    Add files via upload

--- Task1/log.js
+++ Task1/log.js
@@ -1,6 +1,9 @@
-// loop through array and print it`s elements on the console
-for (var i =0; i < data.length; i++){
-	console.log("data[" + i + "] = " + printString(data[i]));
+// my function will be executed when page is fully loaded
+window.onload = function(){
+	// loop through array and print it`s elements on the console
+	for (var i =0; i < data.length; i++){
+		console.log("data[" + i + "] = " + printString(data[i]));
+	}
 }
 
 // function uses strict equals to test value and type equality and print appropriate message
