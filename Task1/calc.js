cf2275cc284d7b58ade87817be79244095f3b4ce
PhilipKozlov <philip.g.kozlov@gmail.com>
2016-08-10 13:52:43 +0300

    Add files via upload

---
+++ Task1/calc.js
@@ -0,0 +1,25 @@
+// loop through array
+for (var i =0; i < data.length; i++)
+{
+	// check if array element is null or undefined
+	if (data[i]!=null){
+		// check if array element contains only digits
+		if (!isNaN(data[i])){
+			data[i] = Action(data[i]);
+		}
+	}
+}
+
+// perform mathematical actions on array elements according to the task
+function Action(value){
+	if (value == 0){
+		value = +value + 10;
+	}
+	else if (value > 100){
+		value = value - 100;
+	}
+	else if(value < 100){
+		value = +value + 100;
+	}
+	return value;
+}
---
+++ Task1/log.js
@@ -0,0 +1,15 @@
+// loop through array and print it`s elements on the console
+for (var i =0; i < data.length; i++){
+	console.log("data[" + i + "] = " + printString(data[i]));
+}
+
+// function uses strict equals to test value and type equality and print appropriate message
+function printString(value){
+	if (value === undefined){
+		value = "не определено";
+	} 
+	if (value === null){
+		value = "не указанно";
+	} 
+	return value;
+}
