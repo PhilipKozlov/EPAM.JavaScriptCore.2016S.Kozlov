09dee6689470af2c32df4896a9ad6423526a40d7
PhilipKozlov <philip.g.kozlov@gmail.com>
2016-08-11 14:26:37 +0300

    Add files via upload

---
+++ Task2/init.js
@@ -0,0 +1,41 @@
+// array of objects
+var data = [];
+// number of possible object types
+var numOfTypes = 3;
+// base function name
+var funcBaseName = "getCount";
+
+// creates objects
+function CreateObjects(numOfObjects, maxCountValue){
+	for (var i = 0; i < numOfObjects; i++){
+		data[i] = CreateElement(maxCountValue);
+		console.log("type=%d,count=%d", GetType(data[i]), data[i].count);
+	}
+}
+
+// creates random Object with "count" property and "getCount" function
+function CreateElement(maxCountValue){
+	// object "type"
+	var rndType = random(1,numOfTypes);
+	// object count value
+	var rndCount = random(1,maxCountValue);
+	// object to be returned
+	var element = {};
+	element.count = rndCount;
+	element[funcBaseName + rndType] = function(){
+										return this.count;
+									  }
+	return element;
+}
+
+function GetType(obj){
+	for (var i = 0; i < numOfTypes; i++){
+			var funcName = funcBaseName + (i + 1);
+			// if type-specific function is defined return appropriate type
+			if (obj[funcName]){
+				return i + 1;
+			}
+		}
+}
+
+CreateObjects(5, 10);
---
+++ Task2/log.js
@@ -0,0 +1,40 @@
+// creates an empty array
+function CreateEmptyArray(){
+	var array = []
+	for (var i = 0; i < numOfTypes; i++){
+		array[i] = null;
+	}
+	return array;
+}
+
+// calculates the sum of count for each type
+function Sum(){
+	// sum of all counts for each object type
+	var countSum = CreateEmptyArray();
+	// loop through array and calculate the sum of count for each type
+	for (var i = 0; i < data.length; i++){
+		// loop through all types
+		for (var j = 0; j < numOfTypes; j++){
+			var funcName = funcBaseName + (j + 1);
+			var obj = data[i];
+			// if type-specific function is defined calculate sum of count
+			if (obj[funcName]){
+				countSum[j] += obj[funcName]();
+			}
+		}
+	}
+	Print(countSum);
+}
+
+// prints result on console
+function Print(array){
+	for (var i = 0; i < array.length; i++){
+		console.log("count%d=%d", i + 1, array[i]);
+	}
+}
+
+Sum();
+
+
+
+
